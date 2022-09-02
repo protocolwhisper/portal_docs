623c98b6d0120d5232ead63151549454a103cbdb
Jason Vranek <jv@jvair.local>
2022-09-02 15:02:59 -0700

    fix order of contracts in sample code

--- sample_code/VRFClient.sol
+++ sample_code/VRFClient.sol
@@ -22,6 +22,59 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC777/IERC777Recipient.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/utils/introspection/IERC1820Registry.sol";
 
+contract PRTLToken is ERC777 {
+    constructor(uint256 initialSupply, address[] memory defaultOperators)
+        ERC777("Portal Token", "PRTL", defaultOperators)
+    {
+        _mint(msg.sender, initialSupply, "", "");
+    }
+}
+
+contract VRFClientBase {
+    // Reference to the ERC1820 Registry contract available on all chains
+    IERC1820Registry internal constant _ERC1820_REGISTRY =
+        IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+
+    constructor() {
+        // Tell erc1820 registry that this contract can send PRTL 
+        _ERC1820_REGISTRY.setInterfaceImplementer(
+            address(this), // account
+            keccak256("ERC777TokensSender"), // interfaceHash
+            address(this) // implementer
+        );
+        // Tell erc1820 registry that this contract can receive PRTL 
+         _ERC1820_REGISTRY.setInterfaceImplementer(
+            address(this), // account
+            keccak256("ERC777TokensRecipient"), // interfaceHash
+            address(this) // implementer
+        );
+    }
+    
+    // The required interface so this contract can send PRTL
+    function tokensToSend(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) external {
+        // insert logic here to run before contract sends PRTL
+    }
+
+    // The required interface so this contract can receive PRTL
+    function tokensReceived(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) external {
+        // when tokens arrive at this contract…
+    }
+}
+
 contract VRFClient is VRFClientBase {
     address owner;
     uint256 public diceRoll;
@@ -86,58 +139,5 @@ contract VRFClient is VRFClientBase {
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
-    }
-}
-
-contract PRTLToken is ERC777 {
-    constructor(uint256 initialSupply, address[] memory defaultOperators)
-        ERC777("Portal Token", "PRTL", defaultOperators)
-    {
-        _mint(msg.sender, initialSupply, "", "");
-    }
-}
-
-contract VRFClientBase {
-    // Reference to the ERC1820 Registry contract available on all chains
-    IERC1820Registry internal constant _ERC1820_REGISTRY =
-        IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
-
-    constructor() {
-        // Tell erc1820 registry that this contract can send PRTL 
-        _ERC1820_REGISTRY.setInterfaceImplementer(
-            address(this), // account
-            keccak256("ERC777TokensSender"), // interfaceHash
-            address(this) // implementer
-        );
-        // Tell erc1820 registry that this contract can receive PRTL 
-         _ERC1820_REGISTRY.setInterfaceImplementer(
-            address(this), // account
-            keccak256("ERC777TokensRecipient"), // interfaceHash
-            address(this) // implementer
-        );
-    }
-    
-    // The required interface so this contract can send PRTL
-    function tokensToSend(
-        address operator,
-        address from,
-        address to,
-        uint256 amount,
-        bytes calldata userData,
-        bytes calldata operatorData
-    ) external {
-        // insert logic here to run before contract sends PRTL
-    }
-
-    // The required interface so this contract can receive PRTL
-    function tokensReceived(
-        address operator,
-        address from,
-        address to,
-        uint256 amount,
-        bytes calldata userData,
-        bytes calldata operatorData
-    ) external {
-        // when tokens arrive at this contract…
     }
 }
